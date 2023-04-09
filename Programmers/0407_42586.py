1570fdca9ffcbdcdfa6505e15e2db171ce53aed5
freemjstudio <mjwoo0@naver.com>
2023-04-09 22:48:06 +0900

    0409

--- Programmers/0407_42586.py
+++ Programmers/0407_42586.py
@@ -2,5 +2,16 @@
 
 def solution(progresses, speeds):
     answer = []
+    stack = []
+    N = len(progresses)
+    for i in range(N):
+        progress = 100 - progresses[i]
+        if progress % speeds[i] != 0:
+            day = progress // speeds[i] + 1
+            stack.append(day)
+        else:
+            day = progress // speeds[i]
+            stack.append(day)
+    print(stack)
 
     return answer
