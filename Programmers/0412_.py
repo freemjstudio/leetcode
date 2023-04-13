1bc2380b56aea808830046f3374ad00926f6bfce
freemjstudio <mjwoo0@naver.com>
2023-04-13 15:10:34 +0900

    0413

--- Programmers/0412_.py
+++ Programmers/0412_.py
@@ -1,6 +1,16 @@
 # 2141 우체국 https://www.acmicpc.net/problem/2141
+import sys
 
 N = int(input())
+
+data = []
+people = 0
 for _ in range(N):
-    a, b = map(int, input().split())
+    x, a = map(int, input().split())
+    data.append([x, a])
+    people += a
+
+data.sort(key= lambda x:x[0])
+
+
 
