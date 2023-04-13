3413ea5caca443944196e1f0e70779d32314d64d
freemjstudio <mjwoo0@naver.com>
2023-04-13 15:17:50 +0900

    0413

--- Programmers/0412_.py
+++ Programmers/0412_.py
@@ -1,16 +1,22 @@
 # 2141 우체국 https://www.acmicpc.net/problem/2141
 import sys
-
+input = sys.stdin.readline
 N = int(input())
 
 data = []
-people = 0
+total_people = 0
 for _ in range(N):
     x, a = map(int, input().split())
     data.append([x, a])
-    people += a
+    total_people += a
 
 data.sort(key= lambda x:x[0])
 
-
+count = 0
+for i in range(N):
+    t, p = data[i]
+    count += p
+    if count >= total_people/2: # 마을 사람 수가 절반이 넘어가는 그 마을이 정답
+        print(t)
+        break
 
