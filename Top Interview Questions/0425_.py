665aa5edd72d194ba603ccae4d8b250ebb92efc7
freemjstudio <mjwoo0@naver.com>
2023-05-01 12:48:28 +0900

    0501

--- Top Interview Questions/0425_.py
+++ Top Interview Questions/0425_.py
@@ -7,3 +7,12 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        total = 0
+        num1 = 0
+        num2 = 0
+        answer = Optional[ListNode]
+
+        print(l1)
+        print(l2)
+
+        return answer
