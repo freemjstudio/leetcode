0b84390dfef6fdd96671c5c005e702625f97b91f
freemjstudio <mjwoo0@naver.com>
2023-05-04 21:39:03 +0900

    0504

--- Top Interview Questions/0502.py
+++ Top Interview Questions/0502.py
@@ -1,2 +1,4 @@
 # https://leetcode.com/problems/longest-substring-without-repeating-characters/
 # Longest Substring without repeating characters
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
