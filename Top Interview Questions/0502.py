9ead7aedf16726f32a98945210978d03ac7a5838
freemjstudio <mjwoo0@naver.com>
2023-05-05 15:47:51 +0900

    0505 :cat:

--- Top Interview Questions/0502.py
+++ Top Interview Questions/0502.py
@@ -2,3 +2,9 @@
 # Longest Substring without repeating characters
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
+        answer = 0  # maxLength
+        # 반복되는 문자열 구하기
+        n = len(s)
+        for window in range(n, -1, -1):  #
+
+        return answer
