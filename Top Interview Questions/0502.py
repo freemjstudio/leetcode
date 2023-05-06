5026271845f409ff726ff3855e815683615317ee
freemjstudio <mjwoo0@naver.com>
2023-05-06 23:01:50 +0900

    0506 :cat:

--- Top Interview Questions/0502.py
+++ Top Interview Questions/0502.py
@@ -5,15 +5,10 @@ class Solution:
         answer = 1  # maxLength
         # 반복되는 문자열 구하기
         n = len(s)
-        for step in range(1, n + 1):
+        for step in range(n, 0, -1):
             flag = False  # 중복된 문자열 나왔는지 체크하기
             check = ''  # 문자열
             for i in range(0, n, step):
                 check = s[i:i + step]  # 확인할 문자열
-                for j in range(step, n, step):
-                    if check == s[j:j + step]:
-                        flag = True
-                        answer = max(step, answer)
-                        continue
 
         return answer
