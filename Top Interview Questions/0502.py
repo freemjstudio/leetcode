17eeddb91f82a30a21126ba54ab26bce5380826f
freemjstudio <mjwoo0@naver.com>
2023-05-15 23:37:11 +0900

    0515 :cat:

--- Top Interview Questions/0502.py
+++ Top Interview Questions/0502.py
@@ -3,13 +3,19 @@
 
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        answer = 1  # maxLength
+        answer = 0 # maxLength
+
         # 반복되는 문자열 구하기
         n = len(s)
-        for step in range(n, 0, -1):
-            flag = False  # 중복된 문자열 나왔는지 체크하기
-            check = ''  # 문자열
-            for i in range(0, n, step):
-                check = s[i:i + step]  # 확인할 문자열
+
+        for left in range(n-1):
+            temp_set = set()
+            temp_set.add(s[left])
+            for right in range(left+1, n):
+                if s[right] not in temp_set:
+                    temp_set.add(s[right])
+                else:
+                    break
+            answer = max(len(temp_set), answer)
 
         return answer
