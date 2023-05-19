cddec1b90c9170717ff933eb76cc8ea9a91312ad
freemjstudio <mjwoo0@naver.com>
2023-05-19 23:10:06 +0900

    0518 :cat:

--- Programmers/0516.py
+++ Programmers/0516.py
@@ -1,5 +1,22 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/42626
+import heapq
+
+
+def check(arr):
+    for i in arr:
+        if i < k:
+            return False
+    return True
+
 
 def solution(scoville, K):
     answer = 0
+
+    while True:
+
+        answer += 1  # 섞는 횟수 + 1
+
+        if check(scoville):
+            break
+
     return answer
