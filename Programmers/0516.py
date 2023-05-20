e6ddf835ef233e3c4920503d123e66f06fc7a57c
freemjstudio <mjwoo0@naver.com>
2023-05-20 11:52:24 +0900

    0520 :cat:

--- Programmers/0516.py
+++ Programmers/0516.py
@@ -2,21 +2,35 @@
 import heapq
 
 
-def check(arr):
+def check(arr, K):
     for i in arr:
-        if i < k:
+        if i < K:
             return False
     return True
 
 
 def solution(scoville, K):
     answer = 0
-
+    queue = []
+    INF = 10000000
+    # 초기 힙큐 구성
+    for i in scoville:
+        heapq.heappush(queue, i)
+    # print(heapq)
     while True:
-
+        low1 = heapq.heappop(queue)  # heappop 하면 가장 작은 원소 리턴
+        low2 = heapq.heappop(queue)
+        mixed = low1 + low2 * 2
+        heapq.heappush(queue, i)
         answer += 1  # 섞는 횟수 + 1
 
-        if check(scoville):
+        if check(queue, K):
+            break
+        if answer >= INF:
             break
 
-    return answer
+            # 정답 출력
+    if answer >= INF:
+        return -1
+    else:
+        return answer
