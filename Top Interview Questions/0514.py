2aadc9e084af0ea8f79cacc56b6f566b6b445332
freemjstudio <mjwoo0@naver.com>
2023-05-21 21:04:01 +0900

    leetcode 0521

--- Top Interview Questions/0514.py
+++ Top Interview Questions/0514.py
@@ -1,4 +1,22 @@
 # https://leetcode.com/problems/longest-palindromic-substring/
 
+# 역순으로 읽어도 같은 절
 class Solution:
     def longestPalindrome(self, s: str) -> str:
+        answer = 1
+        n = len(s)
+        for left in range(n-1):
+            right = n-1
+            temp = 0
+            while left < right:
+                if s[left] == s[right]:
+                    temp += 2
+                    left += 1
+                    right -= 1
+
+            if s[left] == s[right]:
+                temp += 1
+            answer = max(temp, answer)
+
+
+
