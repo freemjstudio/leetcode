b3872431fb1f9f4d198b24e9155172e2fcaeff8f
freemjstudio <mjwoo0@naver.com>
2023-05-21 21:08:42 +0900

    leetcode 0521

--- Top Interview Questions/0514.py
+++ Top Interview Questions/0514.py
@@ -3,20 +3,27 @@
 # 역순으로 읽어도 같은 절
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        answer = 1
+        answer = ""
         n = len(s)
         for left in range(n-1):
             right = n-1
             temp = 0
+            t_left = left
+            t_right = right
             while left < right:
                 if s[left] == s[right]:
                     temp += 2
                     left += 1
                     right -= 1
+                else:
+                    right -= 1
+                    t_right = right
 
-            if s[left] == s[right]:
+            if s[left] == s[right] and left == right:
                 temp += 1
-            answer = max(temp, answer)
+            if len(answer) < temp:
+                answer = s[t_left:t_right+1]
+        return answer
 
 
 
