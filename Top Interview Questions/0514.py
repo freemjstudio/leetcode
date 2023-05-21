7e36ad42025d2d3cb1be2b38e1f5bb63963d54ee
freemjstudio <mjwoo0@naver.com>
2023-05-21 23:02:26 +0900

    leetcode 0521

--- Top Interview Questions/0514.py
+++ Top Interview Questions/0514.py
@@ -7,28 +7,16 @@ class Solution:
         n = len(s)
         if n <= 1:
             return s
-        for left in range(n):
-            right = n - 1
-            temp = 0
-            t_left = left
-            t_right = right
-            while left < right:
-                if s[left] == s[right]:
-                    temp += 2
-                    left += 1
-                    right -= 1
-                else:
-                    left = t_left
-                    right -= 1
-                    temp = 0
-                    t_right = right
-
-            if len(answer) < temp:
-                answer = s[t_left:t_right + 1]
-
         if len(s) == 2:
             if s[0] == s[1]:
                 answer = s
             else:
                 answer = s[0]
+        for left in range(n-1):
+            for right in range(left+1, n):
+                temp = s[left:right+1]
+                if temp == temp[::-1]:
+                    if len(temp) > len(answer):
+                        answer = temp
+
         return answer
