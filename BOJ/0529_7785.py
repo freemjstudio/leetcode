b572e63b19475356356055458f129cc0f9b6404b
freemjstudio <mjwoo0@naver.com>
2023-05-29 15:48:58 +0900

    pro 0529

---
+++ BOJ/0529_7785.py
@@ -0,0 +1,14 @@
+# https://www.acmicpc.net/problem/7785
+from collections import defaultdict
+# 회사에 있는 사람
+
+d = defaultdict(list)
+n = int(input())
+for _ in range(n):
+    name, command = input().split()
+    if len(d[name]) == 0:
+        d[name].append(command) # 회사에 출입
+    else:
+        if command == "leave":
+            # key 삭제
+            d.pop(name, None) # dictionary 에 Key값이 없으면 None을 리턴한다.
