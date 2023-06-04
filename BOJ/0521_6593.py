8215666ae5944a0f2d88e810f61ed7621fed7adb
freemjstudio <mjwoo0@naver.com>
2023-06-04 17:12:03 +0900

    0604

--- BOJ/0521_6593.py
+++ BOJ/0521_6593.py
@@ -1,8 +1,15 @@
 # https://www.acmicpc.net/problem/6593
 
-
 while True:
     L, R, C = map(int, input().split())
     if L == R == C == 0:
         break # 탈출
+    flag = False
+    answer = 0 # 최소 시간
     for _ in range(L):
+
+    if flag:
+        print(f"Escaped in {answer} minute(s).")
+    else:
+        print("Trapped!")
+
