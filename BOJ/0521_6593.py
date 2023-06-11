5f9aefa3c74bbe2be46b85918861494b1cd60a82
freemjstudio <mjwoo0@naver.com>
2023-06-12 07:31:13 +0900

    0612 - 6593

--- BOJ/0521_6593.py
+++ BOJ/0521_6593.py
@@ -1,11 +1,41 @@
 # https://www.acmicpc.net/problem/6593
 
-import deque from collections
+from collections import deque
 
 # 방향 이동 - 동서남북 , 상하
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
+
+def bfs(building, sf, sx, sy, ef, ex, ey, L, C, R):
+    time = 0  #
+    queue = deque([])
+    queue.append((sf, sx, sy)) # start point
+    while queue:
+
+        f, x, y = queue.pop()
+        if building[x][x][y] == 'E':
+            return time
+
+        if f == ef:  # 목적지 층에 도착 ,
+            for i in range(4):
+                nx = x + dx[i]
+                ny = y + dy[i]
+                if 0 <= nx < R and 0 <= ny < C and building[f][nx][ny] != '#':
+                    queue.append((f, nx, ny))
+
+        else:  # 시작층, 목적지층 아닌 경우
+            for i in range(4):
+                nx = x + dx[i]
+                ny = y + dy[i]
+                if 0 <= nx < R and 0 <= ny < C and building[f][nx][ny] != '#':
+                    queue.append((f, nx, ny))
+            # 상층 이동
+            nf = f + 1
+            if nf < L and building[nf][x][y] != '#':
+                queue.append((nf, x, y))
+
+    return 0
 
 while True:
     L, R, C = map(int, input().split())
@@ -20,7 +50,7 @@ while True:
     ef, ex, ey = 0, 0, 0
 
     for f in range(L):
-        floor = []
+        floor = [] # 이거 2차원 배열로 만들어야 함
         for i in range(R):
             data = input()
             temp = []
@@ -29,43 +59,12 @@ while True:
                     sf, sx, sy = f, i, j
                 if j == 'E':
                     ef, ex, ey = f, i, j
-
-                floor.append(temp)
+                temp.append(j)
+            floor.append(temp)
         building.append(floor)
+    print(building)
 
-
-    def bfs():
-        nonlocal floor
-        time = 0  #
-
-        queue = deque([sf, sx, sy]) # start point
-        while queue:
-
-            f, x, y = queue.pop()
-            if floor[x][x][y] == 'E':
-                return time
-
-            if f == ef: # 목적지 층에 도착 ,
-                for i in range(4):
-                    nx = x + dx[i]
-                    ny = y + dy[i]
-                    if 0 <= nx < R and 0 <= ny < C and floor[f][nx][ny] != '#':
-                        queue.append((f, nx, ny))
-
-            else # 시작층, 목적지층 아닌 경우
-                for i in range(4):
-                    nx = x + dx[i]
-                    ny = y + dy[i]
-                    if 0 <= nx < R and 0 <= ny < C and floor[f][nx][ny] != '#':
-                        queue.append((f, nx, ny))
-                # 상층 이동
-                nf = f + 1
-                if nf < L and floor[nf][x][y] != '#':
-                    queue.append((nf, x, y))
-
-        return 0
-
-    answer = bfs()
+    answer = bfs(building, sf, sx, sy, ef, ex, ey, L, C, R)
     if answer:
         print(f"Escaped in {answer} minute(s).")
     else:
