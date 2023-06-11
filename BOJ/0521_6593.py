2ee3371c7172b480f49a3731273732533e9aac42
freemjstudio <mjwoo0@naver.com>
2023-06-12 07:21:18 +0900

    0612 - 6593

--- BOJ/0521_6593.py
+++ BOJ/0521_6593.py
@@ -1,20 +1,18 @@
 # https://www.acmicpc.net/problem/6593
 
+import deque from collections
+
 # 방향 이동 - 동서남북 , 상하
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
-
-def bfs():
-
 
 
 while True:
     L, R, C = map(int, input().split())
     if L == R == C == 0:
-        break # 탈출
-    flag = False
-    answer = 0 # 최소 시간
+        break # test case end
 
+    answer = 0 # 최소 시간
     building = [] # 전체 빌딩 정보
 
     # 출발점 & 탈출점
@@ -35,11 +33,40 @@ while True:
                 floor.append(temp)
         building.append(floor)
 
-    # bfs
 
+    def bfs():
+        nonlocal floor
+        time = 0  #
 
+        queue = deque([sf, sx, sy]) # start point
+        while queue:
 
-    if flag:
+            f, x, y = queue.pop()
+            if floor[x][x][y] == 'E':
+                return time
+
+            if f == ef: # 목적지 층에 도착 ,
+                for i in range(4):
+                    nx = x + dx[i]
+                    ny = y + dy[i]
+                    if 0 <= nx < R and 0 <= ny < C and floor[f][nx][ny] != '#':
+                        queue.append((f, nx, ny))
+
+            else # 시작층, 목적지층 아닌 경우
+                for i in range(4):
+                    nx = x + dx[i]
+                    ny = y + dy[i]
+                    if 0 <= nx < R and 0 <= ny < C and floor[f][nx][ny] != '#':
+                        queue.append((f, nx, ny))
+                # 상층 이동
+                nf = f + 1
+                if nf < L and floor[nf][x][y] != '#':
+                    queue.append((nf, x, y))
+
+        return 0
+
+    answer = bfs()
+    if answer:
         print(f"Escaped in {answer} minute(s).")
     else:
         print("Trapped!")
