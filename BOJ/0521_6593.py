002c3784027621d16ce35e93aa2aa44a10a9d95a
freemjstudio <mjwoo0@naver.com>
2023-06-17 19:42:31 +0900

    Boj 6593 - 230617

--- BOJ/0521_6593.py
+++ BOJ/0521_6593.py
@@ -2,48 +2,34 @@
 
 from collections import deque
 
-# 방향 이동 - 동서남북 , 상하
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+# 방향 이동 - 동서남북 + 상하
+dx = [-1, 1, 0, 0, 0, 0]
+dy = [0, 0, -1, 1, 0, 0]
+dz = [0, 0, 0, 0, -1, 1]
 
-def bfs(building, sf, sx, sy, ef, ex, ey, L, C, R):
-    time = 0  #
-    queue = deque([])
-    queue.append((sf, sx, sy)) # start point
+def bfs():
+    queue = deque()
+    queue.append([sf, sx, sy]) # start point
     while queue:
         f, x, y = queue.pop()
-        if building[f][x][y] == 'E':
-            return time
 
-        if f == ef:  # 목적지 층에 도착
-            for i in range(4):
-                nx = x + dx[i]
-                ny = y + dy[i]
-                if 0 <= nx < R and 0 <= ny < C and building[f][nx][ny] != '#':
-                    queue.append((f, nx, ny))
-        else:  # 시작층, 목적지층 아닌 경우
-            for i in range(4):
-                nx = x + dx[i]
-                ny = y + dy[i]
-                if 0 <= nx < R and 0 <= ny < C and building[f][nx][ny] != '#':
-                    queue.append((f, nx, ny))
-            # 상층 이동
-            nf = f + 1
-            if nf < L and building[nf][x][y] != '#':
-                queue.append((nf, x, y))
+        for i in range(6):
+            nf, nx, ny = f + dz[i], x + dx[i], y + dy[i]
+            if 0 <= nf < L and 0 <= nx < R and 0 <= ny < C and not visited[nf][nx][ny]:
+                if building[nf][nx][ny] == "." or building[nf][nx][ny] == "E": # 통과할 수 있는 경우
+                    dp[nf][nx][ny] = dp[f][x][y] + 1
+                    visited[nf][nx][ny] = 1
+                    queue.append([nf, nx, ny])
 
-    return 0
 
 while True:
-    line = input()
-    if len(line) == 0:
-        continue
-    else:
-        L, R, C = map(int, line.split())
-    if L == R == C == 0:
+    L, R, C = map(int, input().split())
+    if L == 0:
         break # test case end
 
     answer = 0 # 최소 시간
+    dp = [[[0] * C for _ in range(R)] for _ in range(L)] # 최소 시간 정보 저장
+    visited = [[[0] * C for _ in range(R)] for _ in range(L)] # 방문한 칸 기록
     building = [] # 전체 빌딩 정보
 
     # 출발점 & 탈출점
@@ -66,11 +52,10 @@ while True:
                 continue
             floor.append(temp)
         building.append(floor)
- #   print(building)
 
-    answer = bfs(building, sf, sx, sy, ef, ex, ey, L, C, R)
-    if answer:
-        print(f"Escaped in {answer} minute(s).")
+    bfs()
+    if dp[ef][ex][ey] != 0:
+
+        print(f"Escaped in {dp[ef][ex][ey]} minute(s).")
     else:
         print("Trapped!")
-
