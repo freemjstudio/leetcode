da0da76645fbe7bf8329518b150a2636cd5c346b
freemjstudio <mjwoo0@naver.com>
2023-06-18 10:39:57 +0900

    Boj 6593 - 230618

--- BOJ/0521_6593.py
+++ BOJ/0521_6593.py
@@ -1,5 +1,3 @@
-# https://www.acmicpc.net/problem/6593
-
 from collections import deque
 
 # 방향 이동 - 동서남북 + 상하
@@ -9,17 +7,18 @@ dz = [0, 0, 0, 0, -1, 1]
 
 def bfs():
     queue = deque()
-    queue.append([sf, sx, sy]) # start point
+    queue.append([sf, sy, sx]) # start point
+    visited[sf][sy][sx] = 1
     while queue:
-        f, x, y = queue.pop()
+        f, y, x = queue.pop()
 
         for i in range(6):
-            nf, nx, ny = f + dz[i], x + dx[i], y + dy[i]
-            if 0 <= nf < L and 0 <= nx < R and 0 <= ny < C and not visited[nf][nx][ny]:
-                if building[nf][nx][ny] == "." or building[nf][nx][ny] == "E": # 통과할 수 있는 경우
-                    dp[nf][nx][ny] = dp[f][x][y] + 1
-                    visited[nf][nx][ny] = 1
-                    queue.append([nf, nx, ny])
+            nf, ny, nx = f + dz[i], y + dy[i], x + dx[i]
+            if 0 <= nf < L and 0 <= nx < C and 0 <= ny < R and not visited[nf][ny][nx]:
+                if building[nf][ny][nx] == "." or building[nf][ny][nx] == "E": # 통과할 수 있는 경우
+                    dp[nf][ny][nx] = dp[f][y][x] + 1
+                    visited[nf][ny][nx] = 1
+                    queue.append([nf, ny, nx])
 
 
 while True:
@@ -27,35 +26,28 @@ while True:
     if L == 0:
         break # test case end
 
-    answer = 0 # 최소 시간
-    dp = [[[0] * C for _ in range(R)] for _ in range(L)] # 최소 시간 정보 저장
-    visited = [[[0] * C for _ in range(R)] for _ in range(L)] # 방문한 칸 기록
-    building = [] # 전체 빌딩 정보
+    dp = [[[0] * C for i in range(R)] for _ in range(L)] # 최소 시간 정보 저장
+    visited = [[[0] * C for i in range(R)] for _ in range(L)] # 방문한 칸 기록
+    building = [[] * R for i in range(L)] # 전체 빌딩 정보
 
     # 출발점 & 탈출점
     sf, sx, sy = 0, 0, 0
     ef, ex, ey = 0, 0, 0
 
-    for f in range(L):
-        floor = [] # 이거 2차원 배열
-        for i in range(R+1):
-            data = input() # S....
-            temp = [] # ['S', '.', '.', '.', '.']
-            for j in range(len(data)):
-                if data[j] == 'S':
-                    sf, sx, sy = f, i, j
-                if data[j] == 'E':
-                    ef, ex, ey = f, i, j
-                temp.append(data[j])
-
-            if len(temp) == 0:
-                continue
-            floor.append(temp)
-        building.append(floor)
-
+    for i in range(L):
+        for j in range(R):
+            building[i].append(list(map(str, input())))
+        input()
+    for i in range(L):
+        for j in range(R):
+            for k in range(C):
+                if building[i][j][k] == "S":
+                    sf, sy, sx = i, j, k
+                elif building[i][j][k] == "E":
+                    ef, ey, ex = i, j, k
     bfs()
-    if dp[ef][ex][ey] != 0:
 
-        print(f"Escaped in {dp[ef][ex][ey]} minute(s).")
-    else:
+    if dp[ef][ey][ex] == 0:
         print("Trapped!")
+    else:
+        print("Escaped in %d minute(s)." % dp[ef][ey][ex])
