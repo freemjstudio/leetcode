87575c1a80821871191c2d7e86d5c9132807bc46
freemjstudio <mjwoo0@naver.com>
2023-06-19 23:22:04 +0900

    Boj 6593 - 230610

--- Programmers/0530_아이템줍기.py
+++ Programmers/0530_아이템줍기.py
@@ -1,2 +1,7 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/87694
 
+def solution(rectangle, characterX, characterY, itemX, itemY):
+    answer = 0
+    board = [[0] * 50 for _ in range(50)]  # 전체 배열
+
+    return answer
