55e60bbd20d35761103f8ee36d981a007848b0b6
freemjstudio <mjwoo0@naver.com>
2023-06-20 23:12:59 +0900

    Programmers - 230620

--- Programmers/0530_아이템줍기.py
+++ Programmers/0530_아이템줍기.py
@@ -1,4 +1,3 @@
-# https://school.programmers.co.kr/learn/courses/30/lessons/87694
 from collections import deque
 
 dx = [-1, 1, 0, 0]
@@ -8,29 +7,33 @@ dy = [0, 0, -1, 1]
 def solution(rectangle, characterX, characterY, itemX, itemY):
     answer = 0
 
-    board = [[0] * 50 for _ in range(50)]
-    visited = [[0] * 50 for _ in range(50)]
-    for x1, y1, x2, y2 in rectangle:
+    board = [[-1] * 102 for _ in range(102)]
+    visited = [[0] * 102 for _ in range(102)]
+    for rec in rectangle:
+        x1, y1, x2, y2 = map(lambda x: x * 2, rec)
         for i in range(x1, x2 + 1):
             for j in range(y1, y2 + 1):
                 if x1 < i < x2 and y1 < j < y2:
-                    board[i][j] = 2
-                else:
+                    board[i][j] = 0
+                elif board[i][j] != 0:  # 이미 직사각형 안이 아니면서, 테두리로 그릴 수 있는 경우 -> 직사각형끼리 겹치는 경우 방지
                     board[i][j] = 1  # 테두리 그리기
 
     def bfs():
         queue = deque()
-        queue.append([characterX, characterY])  # start
-        visited[characterX][characterY] = 1  # 방문 표시
+        queue.append([characterX * 2, characterY * 2])  # start
+        visited[characterX * 2][characterY * 2] = 1  # 방문 표시
         while queue:
             x, y = queue.popleft()
-
+            if x == itemX * 2 and y == itemY * 2:
+                answer = (visited[x][y] - 1) // 2
+                return answer
+                break
             for i in range(4):
                 nx, ny = x + dx[i], y + dy[i]
-                if 0 <= nx < 50 and 0 <= ny < 50 and board[nx][ny] == 1 and visited[nx][ny] == 0:  # 테두리인 경우에만 이동 가능
+                if board[nx][ny] == 1 and visited[nx][ny] == 0:  # 테두리인 경우에만 이동 가능
                     visited[nx][ny] = visited[x][y] + 1
                     queue.append([nx, ny])
 
-    bfs()
-    answer = visited[itemX][itemY]
+    answer = bfs()
+
     return answer
