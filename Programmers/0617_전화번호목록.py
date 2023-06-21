0c551cc18b0748e6b42fc7a34a4ada0779931c8f
freemjstudio <mjwoo0@naver.com>
2023-06-22 07:41:15 +0900

    Programmers - 230622

--- Programmers/0617_전화번호목록.py
+++ Programmers/0617_전화번호목록.py
@@ -1,2 +1,13 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/42577
 
+def solution(phone_book):
+    answer = True
+    phone_book.sort()
+    n = len(phone_book)
+    for i in range(n-1): # 앞 뒤 비교하기
+        if len(phone_book[i]) < len(phone_book[i+1]):
+            m = len(phone_book[i])
+            if phone_book[i+1][:m] == phone_book[i]:
+                answer = False
+                return answer
+    return answer
