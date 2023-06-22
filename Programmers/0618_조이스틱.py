80eb7799dbc24dcb5272d6f18d15d8e7a0079791
freemjstudio <mjwoo0@naver.com>
2023-06-23 08:20:50 +0900

    Programmers - 230623

--- Programmers/0618_조이스틱.py
+++ Programmers/0618_조이스틱.py
@@ -1,8 +1,28 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/42860
 
+def check():
+    if name == start:
+        return True
+    return False
+
+
+# A : 65, Z: 90
 def solution(name):
     answer = 0
     n = len(name)
     start = n * 'A'
+    name2num = []
+
+    for i in range(n):
+        name2num.append(ord(name[i]))
+    print(name2num)
+
+    pointer = 0
+    while True:
+        if check():
+            break
+
+        # 왼쪽 방향 오른쪽 방향 비교하기
+        left_cnt, right_cnt = 0, 0
 
     return answer
