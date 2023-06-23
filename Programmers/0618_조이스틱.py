64e6aea182f80af7be4ed19380239a3199e1474d
freemjstudio <mjwoo0@naver.com>
2023-06-23 23:42:04 +0900

    Programmers - 230623

--- Programmers/0618_조이스틱.py
+++ Programmers/0618_조이스틱.py
@@ -1,28 +1,41 @@
-# https://school.programmers.co.kr/learn/courses/30/lessons/42860
 
-def check():
-    if name == start:
-        return True
-    return False
-
-
-# A : 65, Z: 90
 def solution(name):
     answer = 0
     n = len(name)
-    start = n * 'A'
-    name2num = []
+
+    # 상 / 하 비교 하여 mininum 값 구하기
+    # Z 의 경우 COUNT 1로 취급해야 함
+    left, right = 0, 0
 
     for i in range(n):
-        name2num.append(ord(name[i]))
-    print(name2num)
-
-    pointer = 0
-    while True:
-        if check():
+        m = min(ord(name[i]) - ord('A'), ord('Z') - ord(name[i]) + 1)
+        right += m
+        print(i, name[i], ord(name[i]), m)
+        if name[i:] == ( n -i) * 'A':
+            print("right")
+            print(name[i:])
             break
 
-        # 왼쪽 방향 오른쪽 방향 비교하기
-        left_cnt, right_cnt = 0, 0
+        right += 1
 
+    # 첫번쨰 위치에서 바로 왼쪽으로 이동하는 경우 처리
+    print()
+    for j in range(n, -1, -1):
+        m = min(ord(name[i]) - ord('A'), ord('Z') - ord(name[i]) + 1)
+        left += m
+        print(i, name[i], ord(name[i]), m)
+        if name[:i] == ( n -i) * 'A':
+            print("left")
+            print(name[:i])
+            break
+        left += 1
+
+
+    # 예외 BAA, AAB
+    # AAA -> JAN A->J, A, A->N
+    # AAAAAA -> JEROEN 이 경우는 어처피 좌우 상관 없음
+
+
+    # 좌 / 우 비교
+    answer = min(left, right)
     return answer
