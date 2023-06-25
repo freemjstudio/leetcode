a71b426d8ef2b16b0de0a8ed66c552ad28814314
freemjstudio <mjwoo0@naver.com>
2023-06-26 07:12:43 +0900

    Programmers - 230626

--- Programmers/0626_프로세스.py
+++ Programmers/0626_프로세스.py
@@ -1,1 +1,9 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/42587
+from collections import deque
+
+
+def solution(priorities, location):
+    answer = 0
+    queue = deque([priorities])
+
+    return answer
