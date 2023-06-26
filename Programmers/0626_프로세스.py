da152d9c440168c8fcb9866ac3aeee0761aa8fa8
freemjstudio <mjwoo0@naver.com>
2023-06-26 21:34:02 +0900

    Programmers - 230626

--- Programmers/0626_프로세스.py
+++ Programmers/0626_프로세스.py
@@ -3,20 +3,24 @@ from collections import deque
 
 
 def solution(priorities, location):
-    answer = 0
-    queue = deque([priorities])
-    n = len(priorities)
-    point = 0
+    answer = 1  # 첫번째 수행 count
+    queue = deque(priorities)
+    idx = location  # process의 위치를 저장
 
-    for i in range(n):
-        if i < location:  # 왼쪽 앞에 있는 경우
-            if priorities[i] >= priorities[location]:
+    while True:
+        now = queue.popleft()
+        if now < max(queue):  # 더 우선순위가 높은 것이 있을 떄
+            queue.append(now)
+            if idx == 0:  # 자기 자신인 경우
+                idx = len(queue) - 1  # 다시 맨 뒤로 보냄
+            else:  # 1이상인 경우
+                idx -= 1
+
+        else:  # now 가 가장 높을 때
+            if idx == 0:  # now 가 location이 가리키는 값일 때
+                return answer
+            else:
                 answer += 1
-        elif i > location:
-            if priorities[i] > priorities[location]:
-                answer += 1
-        else:
-            # i == location
-            continue
+                idx -= 1  # location 을 한칸 앞으로 땡긴다
 
     return answer
