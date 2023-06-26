de5c81033dd99cb5c3c175d21f5491ef76f9fe22
freemjstudio <mjwoo0@naver.com>
2023-06-27 07:04:48 +0900

    Programmers - 230627

--- Programmers/0627_가장먼노드.py
+++ Programmers/0627_가장먼노드.py
@@ -1,1 +1,10 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/49189
+INF = int(1e9)
+
+
+def solution(n, edge):
+    answer = 0
+    graph = [[] for _ in range(n + 1)]  # node 1 ~ n 까지
+    distance = [INF] * (n + 1)
+
+    return answer
