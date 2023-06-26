e0104e3615f93a55b6b457bd9e75f848aeb7d6ad
freemjstudio <mjwoo0@naver.com>
2023-06-26 20:52:37 +0900

    Programmers - 230626

--- Programmers/0626_프로세스.py
+++ Programmers/0626_프로세스.py
@@ -5,5 +5,18 @@ from collections import deque
 def solution(priorities, location):
     answer = 0
     queue = deque([priorities])
+    n = len(priorities)
+    point = 0
+
+    for i in range(n):
+        if i < location:  # 왼쪽 앞에 있는 경우
+            if priorities[i] >= priorities[location]:
+                answer += 1
+        elif i > location:
+            if priorities[i] > priorities[location]:
+                answer += 1
+        else:
+            # i == location
+            continue
 
     return answer
