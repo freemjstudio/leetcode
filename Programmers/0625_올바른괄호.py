d00f2c029ad733b834c9773a2b520be4ce785cb0
freemjstudio <mjwoo0@naver.com>
2023-06-27 06:54:15 +0900

    Programmers - 230626

--- Programmers/0625_올바른괄호.py
+++ Programmers/0625_올바른괄호.py
@@ -1,2 +1,21 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/12909
 
+def solution(s):
+
+    stack = []
+    for i in s:
+        if i == "(":
+            stack.append(i)
+        else:
+            # )
+            if len(stack) == 0:
+                return False
+            elif stack[-1] == "(":
+                stack.pop()
+            elif stack[-1] == ")":
+                return False
+
+    if len(stack) > 0:
+        return False
+
+    return True
