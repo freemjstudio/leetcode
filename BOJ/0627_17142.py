83b6eff1299e78b56c58ac6c63b42ed8ffcf8a50
freemjstudio <mjwoo0@naver.com>
2023-06-30 06:59:45 +0900

    Boj - 230630

--- BOJ/0627_17142.py
+++ BOJ/0627_17142.py
@@ -5,30 +5,37 @@ from collections import deque
 from itertools import combinations
 input = sys.stdin.readline
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+dx = [-1, 0, 1, 0]
+dy = [0, -1, 0, 1]
 
 n, m = map(int, input().split())
 array = []
-answer = int(1e9) # 총 소요 시간
+
+INF = int(1e9)
+answer = INF # 총 소요 시간
 wall = 0 # 벽의 개수
 virus_pos = [] # 2 번 좌표값
-
+empty = 0 # 빈칸
 for i in range(n):
     data = list(map(int, input().split()))
     array.append(data)
     for j in range(n):
         if data[j] == 2:
-            virus_pos.append([i, j])
+            virus_pos.append((i, j))
         elif data[j] == 1:
             wall += 1
+        else:
+            empty += 1
+if empty == 0:
+    print(0)
+    exit(0)
 
 def bfs(v_list):
     visited = [[-1] * n for _ in range(n)]
     queue = deque([])
     for i in range(m):
         queue.append([v_list[i][0], v_list[i][1]])
-        visited[v_list[i][0]][v_list[i][1]] += 1
+        visited[v_list[i][0]][v_list[i][1]] = 0
     time = 0
     while queue:
         x, y = queue.popleft()
@@ -50,13 +57,12 @@ def bfs(v_list):
     if check == wall:
         return time
     else:
-        return int(1e9)
+        return INF
 
 for comb in combinations(virus_pos, m):
-    result = bfs(comb)
-    answer = min(result, answer)
+    answer = min(bfs(comb), answer)
 
-if answer == int(1e9):
-    print(-1)
-else:
+if answer != INF:
     print(answer)
+else:
+    print(-1)
