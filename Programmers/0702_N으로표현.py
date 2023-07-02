7ee1ecca18e86d9e6e463ba9295526ecb412c0f8
freemjstudio <mjwoo0@naver.com>
2023-07-02 12:45:06 +0900

    Programmers 0702

--- Programmers/0702_N으로표현.py
+++ Programmers/0702_N으로표현.py
@@ -3,22 +3,24 @@ def solution(N, number):
     if N == number:
         return 1
 
-    dp = [[0], [N]]  # N을 i 번 사용한 경우 , 계산한 결과 값 저장
-    for i in range(2, 9):
+    dp = []  # N을 i 번 사용한 경우 , 계산한 결과 값 저장
+    for i in range(1, 9):
         temp_dp = set()  # N을 i번 쓰는 경우 임시 저장 테이블
         temp_dp.add(int(str(N) * i))  # ex. 5 -> 55
+
         # 사칙연산
+        for j in range(i - 1):
+            for x in dp[j]:  # i개 이전의 dp 값에서 꺼내오기
+                for y in dp[-j - 1]:  #
+                    temp_dp.add(x + y)
+                    temp_dp.add(x - y)
+                    temp_dp.add(x * y)
 
-        for x in dp[i - 1]:
-            temp_dp.add(x + N)
-            temp_dp.add(x - N)
-            temp_dp.add(x * N)
-
-            if x != 0:
-                temp_dp.add(x / N)
+                    if y != 0:
+                        temp_dp.add(x / y)
 
         if number in temp_dp:
             return i
-        dp.append(list(temp_dp))
+        dp.append(temp_dp)
     # 8개 이하에 number 값이 없는 경우
     return -1
