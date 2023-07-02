d2379762d7d1ab5b7afc5f53fe2a7c6e36af9ef7
freemjstudio <mjwoo0@naver.com>
2023-07-02 12:37:07 +0900

    Programmers 0702

--- Programmers/0702_N으로표현.py
+++ Programmers/0702_N으로표현.py
@@ -1,27 +1,24 @@
-# https://school.programmers.co.kr/learn/courses/30/lessons/42895
-
-
-def recur(x, count, number, N):
-    if count > 8:
-        return
-
-    if x == number:
-        return count
-    recur(x + N, count + 1, number, N)
-    recur(x - N, count + 1, number, N)
-    recur(x * N, count + 1, number, N)
-    recur(x / N, count + 1, number, N)
-
-    # 5 -> 55 로 만들기
-    if isinstance(x, int):
-        nx = str(x) + str(N)
-        recur(int(nx), count + 1, number, N)
-
-
 def solution(N, number):
-    answer = 9
+    # 같은 경우
     if N == number:
         return 1
 
-    answer = min(recur(N, 1, number, N), answer)
-    return answer
+    dp = [[0], [N]]  # N을 i 번 사용한 경우 , 계산한 결과 값 저장
+    for i in range(2, 9):
+        temp_dp = set()  # N을 i번 쓰는 경우 임시 저장 테이블
+        temp_dp.add(int(str(N) * i))  # ex. 5 -> 55
+        # 사칙연산
+
+        for x in dp[i - 1]:
+            temp_dp.add(x + N)
+            temp_dp.add(x - N)
+            temp_dp.add(x * N)
+
+            if x != 0:
+                temp_dp.add(x / N)
+
+        if number in temp_dp:
+            return i
+        dp.append(list(temp_dp))
+    # 8개 이하에 number 값이 없는 경우
+    return -1
