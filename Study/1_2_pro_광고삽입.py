f2bed0967292cab2815204dd03d43558311edede
freemjstudio <mjwoo0@naver.com>
2023-07-10 22:17:26 +0900

    0710

---
+++ Study/1_2_pro_광고삽입.py
@@ -0,0 +1,15 @@
+'''
+Lv.2	72414	광고 삽입
+Lv.2	17684	압축
+14499	주사위 굴리기
+'''
+
+
+def solution(msg):
+    answer = []
+    index = 27
+    word_dict = {chr(i): (i - 64) for i in range(65, 91)}
+
+    print(word_dict)
+
+    return answer
