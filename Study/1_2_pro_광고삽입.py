7d53771c8833b3bbab1d76a1b8a05b0f1b08e27c
freemjstudio <mjwoo0@naver.com>
2023-07-10 23:00:51 +0900

    0710

--- Study/1_2_pro_광고삽입.py
+++ Study/1_2_pro_광고삽입.py
@@ -7,9 +7,22 @@ Lv.2	17684	압축
 
 def solution(msg):
     answer = []
-    index = 27
+    word_num = 27
     word_dict = {chr(i): (i - 64) for i in range(65, 91)}
 
-    print(word_dict)
+    n = len(msg)
+    start = 0
+    while True:
+        if start == n - 1:
+            break
+        for end in range(n - 1, start, -1):
+            if msg[start:end + 1] in word_dict:
+                answer.append(word_dict[msg[start:end + 1]])
+            else:  # 없는 경우
+                if end == start:  # 마지막일 때
+                    word_dict[msg[start]] = word_num
+                    word_num += 1
+                else:
+                    continue
 
     return answer
