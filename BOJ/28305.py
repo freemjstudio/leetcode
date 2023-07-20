9e4b4edc961acf1b0045b241f4cac9a199650100
freemjstudio <mjwoo0@naver.com>
2023-07-20 21:24:54 +0900

    0720

---
+++ BOJ/28305.py
@@ -0,0 +1,2 @@
+# https://www.acmicpc.net/problem/28305 세미나 배정
+
--- Study/14499.py
+++ Study/14499.py
@@ -2,3 +2,4 @@ n, m = map(int, input().split())
 for _ in range(n):
     x, y = map(int, input().split())
 data = list(map(int, input().split()))
+
