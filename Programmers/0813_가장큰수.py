11a8bfa701f0650ae7dbfe6c4bafea9cc3c3e403
freemjstudio <mjwoo0@naver.com>
2023-08-13 13:21:20 +0900

    programmers 가장 큰 수

--- Programmers/0813_가장큰수.py
+++ Programmers/0813_가장큰수.py
@@ -2,8 +2,8 @@
 
 from itertools import permutations
 
-from itertools import permutations
-
+''' 
+시간 초과 ~~~ 
 
 def solution(numbers):
     answer = ''
@@ -12,4 +12,36 @@ def solution(numbers):
     for per in permutations(numbers, n):
         new_num = ''.join(map(str, per))
         answer = max(answer, new_num)
+    return answer
+
+'''
+
+from collections import defaultdict
+
+
+def solution(numbers):
+    answer = ''
+    n = len(numbers)
+
+    num_dict = defaultdict(int)  # (key) 길이 4 : (value) 원래 숫자값
+    same_length_num = []  # 길이를 모두 4로 통일 하기
+    max_length = 0
+    for num in numbers:
+        max_length = max(len(str(num)), max_length)
+    for num in numbers:
+        l = len(str(num))
+        if l == max_length:
+            num_dict[num] = num
+            same_length_num.append(num)
+        elif l < max_length:
+            str_num = str(num) + '0' * (4 - max_length)
+            num_dict[int(str_num)] = num
+            same_length_num.append(int(str_num))
+    same_length_num.sort(reverse=True)
+
+    print(same_length_num)
+    # 정렬 후 원래 숫자만 가져오기
+    for s_num in same_length_num:
+        answer += str(num_dict[s_num])
+
     return answer
