2229fc98caa48b171d8ec44a6aad21ac72eeb11f
freemjstudio <mjwoo0@naver.com>
2023-08-20 16:24:38 +0900

    230820 - 2304 창고 다각형 stack

--- BOJ/0820.py
+++ BOJ/0820.py
@@ -1,10 +1,16 @@
 # https://www.acmicpc.net/problem/2304
 N = int(input()) # 기둥 개수
 answer = 0
+
+stack = []
 bars = []
 for _ in range(N):
     L, H = map(int, input().split())
     bars.append([L, H])
 
 bars.sort() # [[2, 4], [4, 6], [5, 3], [8, 10], [11, 4], [13, 6], [15, 8]]
+for l, h in bars:
+    # 자기 자신 height 보다 높은거 나오면 stack 에 넣고 낮은거 나오면 pop 하는 방식으로 구하기
+
+    print(l, h)
 print(answer)
