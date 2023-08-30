8acaae7a66d996e143b90e80223905e12a79c1d1
freemjstudio <mjwoo0@naver.com>
2023-08-30 22:32:57 +0900

    17484 진우의 달 여행

--- BOJ/0828_17484.py
+++ BOJ/0828_17484.py
@@ -1,5 +1,9 @@
 # https://www.acmicpc.net/problem/17484
 
 N, M = map(int, input().split())
+array = []
 for _ in range(N):
     data = list(map(int, input().split()))
+    array.append(data)
+for a in array:
+    print(*a)
