ab10958780722d8593cde720903f34f3808e6c2e
freemjstudio <mjwoo0@naver.com>
2023-09-04 21:02:36 +0900

    1005

--- BOJ/0903_1005.py
+++ BOJ/0903_1005.py
@@ -4,4 +4,8 @@
 T = int(input())
 for _ in range(T):
     n, k = map(int, input().split())
-    data = list(map(int, input().split())) # 건설에 걸리는 시간
+    time = list(map(int, input().split())) # 건설에 걸리는 시간
+    for _ in range(k):
+        x, y = map(int, input().split()) # 건설 순서 x -> y
+    w = int(input()) # 승리하기 위해 건설해야 하는 건물 (반드시 도착할 지점)
+
