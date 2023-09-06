7ed45e58864112b79fa928aa3f112e2cd96786e5
freemjstudio <mjwoo0@naver.com>
2023-09-06 22:07:00 +0900

    pro 0906

---
+++ Programmers/0905_pro_N진수게임.py
@@ -0,0 +1,12 @@
+def change_num(x):
+    num_str = ''
+    # n 진수로 변환하기, 숫자 몇개를 변환하는 것이 좋을까?
+
+    return num_str
+
+
+def solution(n, t, m, p):
+    answer = ''
+    # n 진수로 변환하기
+
+    return answer
