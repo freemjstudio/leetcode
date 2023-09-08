72c4f413e88d724b0fb1137cc19c34e6a768d656
freemjstudio <mjwoo0@naver.com>
2023-09-08 22:27:31 +0900

    스타트 택시

--- BOJ/0908_스타트택시.py
+++ BOJ/0908_스타트택시.py
@@ -1,14 +1,28 @@
 # https://www.acmicpc.net/problem/19238
-
+from collections import deque
 n, m, fuel = map(int, input().split())
 flag = False # -1
 answer = int(1e9)
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
 array = []
 
 for i in range(n): # n*n 배열
     data = list(map(int, input().split()))
     array.append(data)
+sx, sy = map(int, input().split()) # 택시 출발 위치
+
+customer = []
+
+for j in range(m): # 승객 정보
+    ax, ay, bx, by = map(int, input().split())
+    customer.append([ax, ay, bx, by])
+
+def bfs(sx, sy):
+    queue = deque([])
+
 
 if flag:
     print(answer)
