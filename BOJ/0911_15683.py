9c2420f56581e36f285d37b958fc916bbb0f69d5
freemjstudio <mjwoo0@naver.com>
2023-09-12 23:26:42 +0900

    230911

--- BOJ/0911_15683.py
+++ BOJ/0911_15683.py
@@ -2,6 +2,9 @@
 N, M = map(int, input().split())
 answer = N*M # 사각지대 최소크기
 
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
 array = []
 for i in range(N):
     array.append(list(map(int, input().split())))
