ad895e55def60d0583d6971f7676570a29778744
freemjstudio <mjwoo0@naver.com>
2023-09-13 22:50:04 +0900

    230911

--- BOJ/0911_15683.py
+++ BOJ/0911_15683.py
@@ -1,11 +1,35 @@
-# https://www.acmicpc.net/problem/15683
+import sys
+
 N, M = map(int, input().split())
 answer = N*M # 사각지대 최소크기
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-array = []
-for i in range(N):
-    array.append(list(map(int, input().split())))
+type = {1: [(1,0), (-1, 0), (0,1), (0,-1)],
+        2: [],
+        3: [],
+        4: [],
+        5: []} # 상하좌우
 
+board = []
+cctv = [] # [cctv_type, x, y]
+visited = [[False] * M for _ in range(N)]
+
+for i in range(N):
+    data = list(map(int, input().split()))
+    for j in range(M):
+        if 1 <= data[j] <= 5:
+            cctv.append([data[j], i, j])
+    board.append(data)
+
+answer = N*M
+# backtracking
+def dfs(depth):
+    global type, answer
+    if depth == len(cctv):
+
+        return
+
+dfs(0)
+print(answer)
