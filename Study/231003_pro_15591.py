7d67aa9927a84519cf3a7d96ae7fada3bab4a58c
freemjstudio <mjwoo0@naver.com>
2023-10-04 00:44:55 +0900

    15591 MooTubde

--- Study/231003_pro_15591.py
+++ Study/231003_pro_15591.py
@@ -7,28 +7,21 @@ INF = int(1e9)
 for _ in range(N-1): # edge 정보
     p, q, r = map(int, input().split())
     graph[p].append([q, r])
+    graph[q].append([p, r])
 
 def solve(K, V):
     result = 0 # K 이상인 영상 개수
-    usado = [INF] * (N+1) # distance 대신 저장하기
     visited = [False] * (N+1)
-
-    queue = deque([V])
+    queue = deque([[V, INF]]) # node, usado
     visited[V] = True
-    usado[V] = 0
     while queue:
-        now = queue.popleft()
-        for next, r in graph[now]:
-            if not visited[next]:
+        now, now_usado = queue.popleft()
+        for next, next_usado in graph[now]:
+            next_usado = min(now_usado, next_usado)
+            if not visited[next] and next_usado >= K:
+                result += 1
                 visited[next] = True
-                usado[next] = min(usado[next], r)
-                queue.append(next)
-
-    for i in range(1, N+1):
-        if i == V:
-            continue
-        elif usado[i] >= K:
-            result += 1
+                queue.append([next, next_usado])
     return result
 
 # v 와 유사도가 K 이상인 동영상 개수 구하기
