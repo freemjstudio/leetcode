ab3c0552847cfe833688840b298c980278cd7bf7
freemjstudio <mjwoo0@naver.com>
2023-10-03 15:20:49 +0900

    15591

--- Study/230928_10986_나머지합.py
+++ Study/230928_10986_나머지합.py
@@ -1,17 +1,20 @@
 # https://www.acmicpc.net/problem/10986
+import sys
+
+input = sys.stdin.readline
 N, M = map(int, input().split())
 data = list(map(int, input().split()))
 answer = 0
-dp = [[0] * (N+1) for _ in range(N+1)] # 누적합 저장 dp[i][j] 는 i ~ j 까지의 합
-
-# 누적합 저장하기
+dp = [0] * M # 나머지를 저장하는 배열 , size가 M 이 된다.
+prefix_sum = 0
 for i in range(N):
-    for j in range(i, N):
-        if i == j:
-            dp[i][j] = data[i]
-        else:
-            dp[i][j] = dp[i][j-1] + data[j]
+    prefix_sum += data[i]
+    dp[prefix_sum%M] += 1 # 나머지가 몇개인지 저장하기
 
-# 누적합을 M 으로 나누기
+# 누적합을 M 으로 나누기,
+answer = dp[0] # 나머지가 0 인 경우는 무조건 포함시키기
 
+# iC2 조합으로 구하기 , 나머지가 같은 구간 2개를 뽑는 모든 경우의 수
+for i in dp:
+    answer += i*(i-1)//2
 print(answer)
---
+++ Study/231003_pro_15591.py
@@ -0,0 +1,5 @@
+# https://www.acmicpc.net/problem/15591
+
+N, Q = map(int, input().split())
+for _ in range(N):
+    p, q, r = map(int, input().split())
