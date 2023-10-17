f4fb9784cc39e5236fd132d664cba69dba4e1f51
freemjstudio <mjwoo0@naver.com>
2023-10-17 21:25:33 +0900

    1253 좋다.py

---
+++ BOJ/231017_1253_좋다.py
@@ -0,0 +1,6 @@
+# https://www.acmicpc.net/problem/1253
+answer = 0
+N = int(input())
+data = list(map(int, input().split()))
+
+print(answer)
--- Programmers/1015_파괴되지않은건물.py
+++ Programmers/1015_파괴되지않은건물.py
@@ -1,13 +1,19 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/92344
+
 def solution(board, skill):
     answer = 0
     n = len(board)
     m = len(board[0])
 
-    for t, r1, c1, r2, c2 in skill:
+    for t, r1, c1, r2, c2, degree in skill:
         if t == 1:  # 파괴
-
+            for i in range(r1, r2 + 1):
+                for j in range(c1, c2 + 1):
+                    board[i][j] -= degree
         else:  # 회복
+            for i in range(r1, r2 + 1):
+                for j in range(c1, c2 + 1):
+                    board[i][j] += degree
 
     # 파괴되지 않은 건물
     for i in range(n):
