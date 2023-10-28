b6fbd918b8fac87fb3e00e19965cce5f18401b7c
freemjstudio <mjwoo0@naver.com>
2023-10-28 19:25:20 +0900

    [1261] 알고 스팟

--- BOJ/231024_11660_구간합구하기5.py
+++ BOJ/231024_11660_구간합구하기5.py
@@ -1,25 +1,21 @@
 # https://www.acmicpc.net/problem/11660
 
 N, M = map(int, input().split())
-board = []
-
-def get_total(x1, y1, x2, y2):
-    total = 0 # 총 누적합 구하기
-
-    for i in range(x1, x2+1):
-        for j in range(y1, y2+1):
-            total += board[i][j]
-
-    return total
+board = [[0] * (N+1)]
 
 for _ in range(N):
-    board.append(list(map(int, input().split())))
+    board.append([0] + list(map(int, input().split())))
 
 # 미리 누적합 구해놓기
-for i in range(N):
-    for j in range(N):
-        board[i][j] += board[i][j-1]
+for i in range(1, N+1):
+    for j in range(1, N):
+        board[i][j+1] += board[i][j]
+
+for i in range(1, N):
+    for j in range(1, N+1):
+        board[i+1][j] += board[i][j]
 
 for _ in range(M):
     x1, y1, x2, y2 = map(int, input().split())
-    print(get_total(x1-1, y1-1, x2-1, y2-1))
+    total = board[x2][y2] - board[x2][y1-1] - board[x1-1][y2] + board[x1-1][y1-1]
+    print(total)
