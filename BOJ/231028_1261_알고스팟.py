c6c4036a98457e61386fcf8764913f04c7d1a877
freemjstudio <mjwoo0@naver.com>
2023-11-07 00:16:44 +0900

    1459

--- BOJ/231028_1261_알고스팟.py
+++ BOJ/231028_1261_알고스팟.py
@@ -6,12 +6,7 @@ N, M = map(int, input().split())
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-board = []
-answer = N*M
-
-for _ in range(N):
-    data = list(input())
-    board.append(data)
+board = [list(map(int, input())) for _ in range(N)]
 
 queue = deque([])
 queue.append([0, 0]) # x, y
@@ -21,23 +16,17 @@ distance = [[-1] * M for _ in range(N)]
 distance[0][0] = 0
 while queue:
     x, y = queue.popleft()
-
-    if x == N-1 and y == M-1: # 도착점
-        answer = min(answer, distance[x][y])
-
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
 
         if 0 <= nx < N and 0 <= ny < M:
-            if board[nx][ny] == '1':
-                if distance[nx][ny] == -1: # 방문 처리 안된 경우
+            if distance[nx][ny] == -1: # 아직 방문하지 않은 칸 등장
+                if board[nx][ny] == 0: # 통로
+                    distance[nx][ny] = distance[x][y]
+                    queue.append([nx, ny])
+                else: # 벽
                     distance[nx][ny] = distance[x][y] + 1
                     queue.append([nx, ny])
-            else: # board[nx][ny] == '0'
-                if distance[nx][ny] == -1:
-                    distance[nx][ny] += 1
-                    queue.append([nx, ny])
 
-
-print(answer)
+print(distance[N-1][M-1])
