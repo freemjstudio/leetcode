26afdcfc32980a17de3cbbc9f343bab8e8bb7104
freemjstudio <mjwoo0@naver.com>
2023-11-18 00:13:12 +0900

    [Programmers] 표 병합

--- Programmers/1116_표병합.py
+++ Programmers/1116_표병합.py
@@ -1,59 +1,66 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/150366
 from collections import defaultdict
 
-'''
-["UPDATE 1 1 a", "UPDATE 1 2 b", "UPDATE 2 1 c", "UPDATE 2 2 d", 
-"MERGE 1 1 1 2", "MERGE 2 2 2 1", "MERGE 2 1 1 1", 
-"PRINT 1 1", "UNMERGE 2 2", "PRINT 1 1"]
-'''
-
 
 def solution(commands):
     answer = []
-    table = [[""] * 50 for _ in range(50)]
+    parents = [[(r, c) for r in range(51)] for c in range(51)]
+    table = [["EMPTY"] * 51 for _ in range(51)]
     value_dict = defaultdict(list)
-    merged_dict = defaultdict(list)
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
 
-    # 인접 하는 칸인지 체크하기
-    def check(r1, c1, r2, c2):
-        for i in range(4):
-            nr = r1 + dx[i]
-            nc = r1 + dy[i]
-            if nr == r2 and nr == c2:
-                return True
-        return False
+    # union-find 알고리즘
+    def find_parents(r, c):
+        if parents[r][c] == (r, c):
+            return (r, c)
+        p = parents[r][c]
+        find_parents(p[0], p[1])
+
+    def PRINT(r, c):
+        return table[r][c]
+
+    def UPDATE(r, c, value):
+        table[r][c] = value
+
+    def UPDATE2(value1, value2):
+        # value1 을 가지고 있는 모든 셀 선택하기
+        for i in range(1, 51):
+            for j in range(1, 51):
+                if table[i][j] == value1:
+                    table[i][j] = value2
+
+    def MERGE(r1, c1, r2, c2):
+        if table[r1][c1] != "EMPTY":
+            table[r2][c2] = table[r1][c1]
+            parents[r2][c2] = (r1, c1)
+        else:
+            table[r1][c1] = table[r2][c2]
+            parents[r1][c1] = (r2, c2)
+
+    def UNMERGE(r, c):
+        # parents 찾기
+        p = find_parents(r, c)
+        # parents 에 저장된 값
+        original = table[p[0]][p[1]]
 
     for command in commands:
-        x = command.split()
-        if x[0] == "UPDATE":
-            if len(x) == 4:
-                r, c, value = int(x[1]) - 1, int(x[2]) - 1, x[3]
-                table[r][c] = value
-                value_dict[value].append((r, c))
-            else:
-                old_value, new_value = x[1], x[2]
-                for r1, c1 in value_dict[old_value]:
-                    table[r1][c1] = new_value
-        elif x[0] == "MERGE":
-            r1, c1, r2, c2 = int(x[1]) - 1, int(x[2]) - 1, int(x[3]) - 1, int(x[4]) - 1
+        cmd, *args = command.split()
+        if cmd == "UPDATE":
+            if len(args) == 3:
+                r, c, value = args
+                UPDATE(int(r), int(c), value)
+            else:  # 2
+                value1, value2 = args
+                UPDATE2(value1, value2)
+        elif cmd == "MERGE":
+            r1, c1, r2, c2 = map(int, args)
             if r1 == r2 and c1 == c2:
                 continue
-                # 인접하는 경우
-            if check(r1, c1, r2, c2):
-
-            else:  # 인접하지 않는 경우
-
-
-
-        elif x[0] == "UNMERGE":
-
-        elif x[0] == "PRINT":
-            r, c = int(x[1]) - 1, int(x[2]) - 1
-            if table[r][c]:
-                answer.append(table[r][c])
-            else:
-                answer.append("EMPTY")
+            MERGE(r1, c1, r2, c2)
+        elif cmd == "UNMERGE":
+            r, c = map(int, args)
+            UNMERGE(r, c)
+        elif cmd == "PRINT":
+            r, c = map(int, args)
+            answer.append(PRINT(r, c))
 
     return answer
