c4fc66383fb17d453e64cdfba45c4681dcc2c138
freemjstudio <mjwoo0@naver.com>
2023-12-11 23:11:33 +0900

    [LeetCode] 530

--- Top Interview Questions/[LeetCode] 530. Minimum Absolute Difference.py
+++ Top Interview Questions/[LeetCode] 530. Minimum Absolute Difference.py
@@ -1,2 +1,10 @@
 # https://leetcode.com/problems/minimum-absolute-difference-in-bst/?envType=study-plan-v2&envId=top-interview-150
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def getMinimumDifference(self, root: Optional[TreeNode]) -> int:
