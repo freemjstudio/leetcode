278c3881e8daa507972901426ee687049dc84506
freemjstudio <mjwoo0@naver.com>
2023-12-18 08:43:13 +0900

    79. Word Search

--- Top Interview Questions/[LeetCode] 79. Word Search.py
+++ Top Interview Questions/[LeetCode] 79. Word Search.py
@@ -1,10 +1,9 @@
 # https://leetcode.com/problems/word-search/?envType=study-plan-v2&envId=top-interview-150
-
 from collections import deque
 
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        flag = True
+        flag = False
         N = len(board)
         M = len(board[0])
 
@@ -13,13 +12,17 @@ class Solution:
         dx = [-1, 1, 0, 0]
         dy = [0, 0, -1, 1]
 
-        def dfs(i, j, arr, idx):
+        def dfs(x, y, arr, idx):
             if len(arr) == len(word):
-                return
+                new_word = "".join(arr)
+                if new_word == word:
+                    flag = True
+                    return
             for k in range(4):
                 nx = x + dx[k]
                 ny = y + dy[k]
-                if 0 <= nx < N and 0 <= ny <= M:
+
+                if 0 <= nx < N and 0 <= ny < M:
                     ch = board[nx][ny]
 
                     if word[idx] == ch:
@@ -33,6 +36,5 @@ class Solution:
                 if board[i][j] == s:
                     # backtracking
                     dfs(i, j, [s], 1)
-
 
         return flag
