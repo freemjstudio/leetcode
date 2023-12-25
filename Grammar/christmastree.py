3de5e31c02274997225911498710677847a61eca
freemjstudio <mjwoo0@naver.com>
2023-12-25 15:18:48 +0900

    [LeetCode] LRU Cache

--- Grammar/christmastree.py
+++ Grammar/christmastree.py
@@ -1,5 +1,11 @@
 class Flight:
     class_attr = []
+
+    '''
+    비공개 클래스 속성 
+    언더바 두개로 속성명이 시작하면 비공개 속성이 된다. 
+    '''
+    __private_attr = 5
     '''
     # 첫번쨰 파라미터는 self 라는 이름을 관례적으로 사용한다.
     # 호출 시 호출한 객체 자신이 전달되기 때문에 self 라는 이름을 사용하게 된 것이다.
@@ -8,7 +14,11 @@ class Flight:
     # init 메소드만 오버라이딩하여 객체를 초기화 할 떄 이용된다.
     def __init__(self):
         print("초기화 함수")
-
+    '''
+    class 속성에 직접 접근함. 
+    클래스 속성에 값을 추가함. 
+    클래스명.클래스속성, 객체변수명.클래스속성 
+    '''
     def add_class_attr(self, number):
         Flight.class_attr.append(number)
 
@@ -22,3 +32,6 @@ f = Flight()
 f.add_instance_attr(5)
 f.get_instance()
 
+g = Flight()
+g.add_class_attr(7)
+g.get_instance()
---
+++ Top Interview Questions/[LeetCode] 146. LRU Cache.py
@@ -0,0 +1,49 @@
+# https://leetcode.com/problems/lru-cache/?envType=study-plan-v2&envId=top-interview-150
+
+
+class Node:
+    def __init__(self, key=0, value=0):
+        self.key, self.value = key, value
+        self.prev = self.next = None
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.__capacity = capacity
+        self.__cache = {} # Dictionary Type for O(1)
+        self.left, self.right = Node(), Node()
+        self.left.next = self.right
+        self.right.prev = self.left
+
+    def insert(self, node):
+
+    def remove(self, node):
+
+    def get(self, key: int) -> int:
+        if key in self.__cache: # check the key in the cache
+            # Delete the exist Node
+            self.remove(self.__cache[key])
+            # Insert the new Node
+            self.insert(self.__cache[key])
+            return self.__cache[key].value
+
+        return -1  # If the key doesn't exist
+
+    def put(self, key: int, value: int) -> None:
+        # check the key
+        if key in self.__cache:
+            self.remove(self.__cache[key]) # remove if already exists
+        self.__cache[key] = Node(key, value) # Node 생성하기
+        self.insert(self.__cache[key])
+
+        # capacity check
+        if len(self.__cache) > self.__capacity:
+            # 삭제하기
+            LRU = self.left.next # ?
+            self.remove(LRU)
+            del self.__cache[LRU.key]
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
