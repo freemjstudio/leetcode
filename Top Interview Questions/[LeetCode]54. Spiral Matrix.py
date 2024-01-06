e94b502aada18d5ba053e0e3d633d98989fca134
freemjstudio <mjwoo0@naver.com>
2024-01-06 13:46:45 +0900

    LeetCode:54

---
+++ Top Interview Questions/[LeetCode]54. Spiral Matrix.py
@@ -0,0 +1,30 @@
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        answer = []
+        N = len(matrix)
+        M = len(matrix[0])
+
+        dx = [0, 1, 0, -1]
+        dy = [1, 0, -1, 0]
+        i, j = 0, 0
+        visited = [[False] * M for _ in range(N)]
+        k = 0 # direction
+
+        for _ in range(N*M):
+            answer.append(matrix[i][j])
+
+            i, j = i + dx[k], j + dy[k]
+            if not (0 <= i < N and 0 <= j < M) or visited[i][j]:
+                i, j = i - dx[k], j - dy[k]
+                k = (k+1) % 4
+
+
+
+        return answer
+
+
+# TEST CASE
+matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+s = Solution()
+print(s.spiralOrder(matrix))
+
