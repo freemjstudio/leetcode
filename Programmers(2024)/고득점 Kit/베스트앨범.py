614ad859bc259574e646d2ca44e3495b2dc2ac93
freemjstudio <mjwoo0@naver.com>
2024-01-16 20:53:24 +0900

    Programmers 베스트 엘범

---
+++ Programmers(2024)/고득점 Kit/베스트앨범.py
@@ -0,0 +1,21 @@
+from collections import defaultdict
+
+
+def solution(genres, plays):
+    answer = []
+    n = len(genres)
+    hash_map = defaultdict(int)  # -> 조건 1: 속한 노래가 많이 재생된 장르
+    hash_song = defaultdict(list)  # -> 장르 내에서 많이 재생된 노래 classic : [(500, 0), (150, 2), ...]
+    for i in range(n):
+        hash_map[genres[i]] += plays[i]
+        hash_song[genres[i]].append((plays[i], i))
+    gen_arr = sorted(list(hash_map.items()), reverse=True, key=lambda item: item[1])
+    for gen_item in gen_arr:
+        gen = gen_item[0]
+        sorted_song = sorted(hash_song[gen], reverse=True, key=lambda item: (item[0], -item[1]))
+        for i in range(len(sorted_song)):
+            if i >= 2:
+                break
+            answer.append(sorted_song[i][1])
+
+    return answer
--- Programmers(2024)/고득점 Kit/주식가격.py
+++ Programmers(2024)/고득점 Kit/주식가격.py
@@ -1,5 +1,22 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/42584
 
+from collections import deque
+
+def solution(prices):
+    answer = []
+    queue = deque(prices)
+
+    while queue:
+        price = queue.popleft()
+        count = 0
+        for q in queue:
+            count += 1
+            if q < price:  # r감소
+                break
+        answer.append(count)
+    return answer
+
+'''
 def solution(prices):
     n = len(prices)
     answer = [0 for _ in range(n)]
@@ -11,3 +28,4 @@ def solution(prices):
                 continue
 
     return answer
+'''
